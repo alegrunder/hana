3074069bd040a07a5a0e1e6aca5e5e959c789766
heinipas <heinipas@students.zhaw.ch>
2025-06-07 10:26:19 +0200

    Probleme Topf gelöst

--- hana_project_book/ImportExport/global_parameters.py
+++ hana_project_book/ImportExport/global_parameters.py
@@ -1,17 +1,19 @@
 from ngsolve import *
 
-omega = 2*pi*50     # Frequenz 50 Hz
-mu0 = 4*pi*1e-7     # Magnetische Feldkonstante
+omega = 2*pi*50       # Frequenz 50 Hz
+mu0 = 4*pi*1e-7       # Magnetische Feldkonstante
 
-sigmaCu = 56e6      # S/m (Kupfer)
-sigmaCore = 56e6    # S/m (Kupfer)
+sigmaCu = 56e6        # S/m (Kupfer)
+sigmaCoreTopf = 56e6  # S/m (Core und Topf - Kupfer)
+sigmaWater = 0.01     # S/m (Leitungswasser: 0.005 ... 0.05 S/m)
 
-lamAir = 0.0262     # W/(m K)
-lamCu = 400         # W/(m K)
-lamCore = 400       # W/(m K)
+lamAir = 0.0262       # W/(m K)
+lamCu = 400           # W/(m K)
+lamCoreTopf = 400     # W/(m K) (Core und Topf)
+lamWater = 0.6        # W/(m K)
 
-# Reihenfolge: air, copper, core
-sigma = CoefficientFunction([0 , sigmaCu , sigmaCore ])
-mur = CoefficientFunction([1 ,1 ,1])
+# Reihenfolge: air, copper, core, water
+sigma = CoefficientFunction([0 , sigmaCu , sigmaCoreTopf, sigmaWater])
+mur = CoefficientFunction([1 ,1 ,1, 1])
 nu = CoefficientFunction(1/mu0 * 1/mur ) # Permeabilität des Materials
-lam = CoefficientFunction([lamAir, lamCu, lamCore])
+lam = CoefficientFunction([lamAir, lamCu, lamCoreTopf, lamWater])
